87dc34363e1b218b0613f03d33e1ca5815f016ca
Ayhan-Agoston Daniel <ayhand@regens.hu>
2025-12-02 16:30:52 +0100

    added remaining days + build

--- src/content.js
+++ src/content.js
@@ -1,51 +1,52 @@
 import i01 from "./assets/days/01.webp";
 import i02 from "./assets/days/02.jpeg";
 import i03 from "./assets/days/03.jpeg";
-// import i04 from "./assets/days/04.webp";
-// import i05 from "./assets/days/05.webp";
-// import i06 from "./assets/days/06.webp";
-// import i07 from "./assets/days/07.webp";
-// import i08 from "./assets/days/08.webp";
-// import i09 from "./assets/days/09.webp";
-// import i10 from "./assets/days/10.webp";
-// import i11 from "./assets/days/11.webp";
-// import i12 from "./assets/days/12.webp";
-// import i13 from "./assets/days/13.webp";
-// import i14 from "./assets/days/14.webp";
-// import i15 from "./assets/days/15.webp";
-// import i16 from "./assets/days/16.webp";
-// import i17 from "./assets/days/17.webp";
-// import i18 from "./assets/days/18.webp";
-// import i19 from "./assets/days/19.webp";
-// import i20 from "./assets/days/20.webp";
-// import i21 from "./assets/days/21.webp";
-// import i22 from "./assets/days/22.webp";
-// import i23 from "./assets/days/23.webp";
-// import i24 from "./assets/days/24.webp";
+import i04 from "./assets/days/04.jpeg";
+import i05 from "./assets/days/05.jpeg";
+import i06 from "./assets/days/06.jpeg";
+import i07 from "./assets/days/07.png";
+import i08 from "./assets/days/08.jpeg";
+import i09 from "./assets/days/09.jpeg";
+import i10 from "./assets/days/10.jpeg";
+import i11 from "./assets/days/11.jpg";
+import i12 from "./assets/days/12.jpg";
+import i13 from "./assets/days/13.jpg";
+import i14 from "./assets/days/14.jpeg";
+import i15 from "./assets/days/15.png";
+import i16 from "./assets/days/16.jpg";
+import i17 from "./assets/days/17.jpeg";
+import i18 from "./assets/days/18.jpeg";
+import i19 from "./assets/days/19.jpg";
+import i20 from "./assets/days/20.jpeg";
+import i21 from "./assets/days/21.jpg";
+import i22 from "./assets/days/22.jpeg";
+import i23 from "./assets/days/23.jpeg";
+import i24 from "./assets/days/24.jpg";
+
 
 export const contentsByDay = [
 	{ imgSrc: i01 },
 	{ imgSrc: i02 },
 	{ imgSrc: i03 },
-	// { imgSrc: i04 },
-	// { imgSrc: i05 },
-	// { imgSrc: i06 },
-	// { imgSrc: i07 },
-	// { imgSrc: i08 },
-	// { imgSrc: i09 },
-	// { imgSrc: i10 },
-	// { imgSrc: i11 },
-	// { imgSrc: i12 },
-	// { imgSrc: i13 },
-	// { imgSrc: i14 },
-	// { imgSrc: i15 },
-	// { imgSrc: i16 },
-	// { imgSrc: i17 },
-	// { imgSrc: i18 },
-	// { imgSrc: i19 },
-	// { imgSrc: i20 },
-	// { imgSrc: i21 },
-	// { imgSrc: i22 },
-	// { imgSrc: i23 },
-	// { imgSrc: i24 },
+	{ imgSrc: i04 },
+	{ imgSrc: i05 },
+	{ imgSrc: i06 },
+	{ imgSrc: i07 },
+	{ imgSrc: i08 },
+	{ imgSrc: i09 },
+	{ imgSrc: i10 },
+	{ imgSrc: i11 },
+	{ imgSrc: i12 },
+	{ imgSrc: i13 },
+	{ imgSrc: i14 },
+	{ imgSrc: i15 },
+	{ imgSrc: i16 },
+	{ imgSrc: i17 },
+	{ imgSrc: i18 },
+	{ imgSrc: i19 },
+	{ imgSrc: i20 },
+	{ imgSrc: i21 },
+	{ imgSrc: i22 },
+	{ imgSrc: i23 },
+	{ imgSrc: i24 },
 ]
